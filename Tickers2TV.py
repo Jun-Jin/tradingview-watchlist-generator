3e19921c20ca4dd5c0eb0d3bdd6b77b6708133b5
Jun Jin <shun.kin.sft@gmail.com>
2022-10-02 22:47:13 +0900

    add TSE symbols

--- Tickers2TV.py
+++ Tickers2TV.py
@@ -2,29 +2,57 @@
 # -*- coding: utf-8 -*-
 
 import ftplib
+import requests
+import pandas as pd
 
-DELIMITER = '|'
 
 def main():
-    nasdaq_list = download_nasdaq_tickers_info_string().split("\n")[1:-2]
-    nasdaq_symbols = extract_symbols_only(nasdaq_list)
-    open('nasdaq_tickers.txt', 'w').write("\n".join(nasdaq_symbols))
+    # Write TSE ticker symbol codes to txt file.
+    tse_xls = download_tse_tickers_info()
+    tse_df = pd.read_excel(tse_xls)
+    tse_codes = tse_df['コード'].astype(str).tolist()
+    write_to_file(
+            fname = 'tse_symbols.txt',
+            code_list = tse_codes,
+            seperator = '\n')
+
+    # Write NASDAQ ticker symbol codes to txt file.
+    nasdaq_list = download_nasdaq_tickers_info_string().split('\n')[1:-2]
+    nasdaq_codes = extract_codes_only(nasdaq_list)
+    write_to_file(
+            fname = 'nasdaq_symbols.txt',
+            code_list = nasdaq_codes,
+            seperator = '\n')
+
 
 def download_nasdaq_tickers_info_string(target = 'nasdaq'):
     # Download & assign to variable from ftp server.
-    ftp = ftplib.FTP(host = 'ftp.nasdaqtrader.com', user = 'anonymous', passwd = 'anonymous@debian.org')
+    # TODO: Also can add others by pass the argumet as "other" or "all".
+    ftp = ftplib.FTP(
+            host = 'ftp.nasdaqtrader.com',
+            user = 'anonymous',
+            passwd = 'anonymous@debian.org')
     ftp.cwd('/SymbolDirectory')
     byte_array = bytearray()
     ftp.retrbinary('RETR ' + "%slisted.txt" % target, byte_array.extend)
     ftp.quit()
 
-    # youcan add others by pass the argumet as "other" or "all".
-
     # To String.
     return byte_array.decode()
 
-def extract_symbols_only(tickers_list):
-    return [l.split(DELIMITER)[0] for l in tickers_list]
+
+def extract_codes_only(tickers_list):
+    return [l.split('|')[0] for l in tickers_list]
+
+
+def download_tse_tickers_info():
+    url = 'https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls'
+    return requests.get(url).content
+
+
+def write_to_file(fname, code_list, seperator=','):
+    open(fname, 'w').write(seperator.join(code_list))
+
 
 if __name__ == '__main__':
     main()
