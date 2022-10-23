51d5b8e39467ba3545a5df1cf518b344481426e1
Jun Jin <shun.kin.sft@gmail.com>
2022-10-23 13:50:11 +0900

    chaneg the way to get market info to json file read

--- watchlist_generator_various.py
+++ watchlist_generator_various.py
@@ -1,77 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
 import os
 from operator import itemgetter
+
+from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
-
-
-# the market informations
-MARKETS = {
-    'nikkei255': {
-        'region' : 'jp',
-        'url': 'https://site1.sbisec.co.jp/ETGate/?OutSide=on&_ControlID=WPLETmgR001Control&_PageID=WPLETmgR001Mdtl20&_DataStoreID=DSWPLETmgR001Control&_ActionID=DefaultAID&getFlg=on&burl=search_market&cat1=market&cat2=none&dir=info&file=market_meigara_225.html',
-        'column': 0,
-    },
-    'nikkei400': {
-        'region': 'jp',
-        'url': 'https://www.sbisec.co.jp/ETGate/?OutSide=on&_ControlID=WPLETmgR001Control&_PageID=WPLETmgR001Mdtl20&_DataStoreID=DSWPLETmgR001Control&_ActionID=DefaultAID&getFlg=on&burl=search_market&cat1=market&cat2=none&dir=info&file=market_meigara_400.html', 
-        'column': 0,
-    },
-    'topix': {
-        'region': 'jp',
-        'url':    'https://search.sbisec.co.jp/v2/popwin/info/stock/pop690_topix100.html',
-        'column': 0,
-    },
-    'sp500': {
-        'region': 'na',
-        'url': 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
-        'column': 0,
-    },
-    'nasdaq100':{
-        'region': 'na',
-        'url': 'https://en.wikipedia.org/wiki/Nasdaq-100',
-        'column': 1,
-    },
-    'dow':  {    
-        'region': 'na',
-        'url': 'https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average',
-        'column': 2,
-    },
-}
-
-MARKETS_LINE = {
-    'line-a': {
-        'region': 'jp',
-        'url': 'https://line-sec.co.jp/contents/stock-1005-A.html',
-        'indices': slice(2, 585),
-    },
-    'line-b': {
-        'region': 'jp',
-        'url': 'https://line-sec.co.jp/contents/stock-1005-B.html',
-        'indices': slice(2, 955),
-    },
-}
 
 
 def main():
+    # read from json files
+    src_dir = os.path.join(os.getcwd(), 'resources', 'market')
+
+    standard = read_json(src_dir, 'standard.json')
+    line = read_json(src_dir, 'line.json')
+
     # create 'output' dir if not exists
     output_dir = os.path.join(os.getcwd(), 'output')
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
     # loop for every market from table html
-    for name, info in MARKETS.items():
+    for name, info in standard.items():
         li = get_symbol_list_from_table_html(name, info)
         write_to_file(output_dir, name, li)
 
     # loop for every market from raw html
-    for name, info in MARKETS_LINE.items():
+    for name, info in line.items():
         li = get_symbol_list_from_raw_html(info)
         write_to_file(output_dir, name, li)
         
+
+def read_json(src_dir, fname):
+    raw_json = open(os.path.join(src_dir, fname), 'r')
+    return json.load(raw_json)
+
 
 def get_symbol_list_from_table_html(name, info):
     """
@@ -84,8 +49,8 @@ def get_symbol_list_from_table_html(name, info):
     """
 
     region, url, column = itemgetter('region', 'url', 'column')(info)
-    tb_list = pd.read_html(url)
 
+    tb_list = pd.read_html(url)
     # get table
     if name == 'topix':
         tb = pd.concat(tb_list[1:3])
@@ -95,6 +60,7 @@ def get_symbol_list_from_table_html(name, info):
         tb = tb_list[4]
     else:
         tb = tb_list[1]
+
     symbol_list = list(tb.iloc[:, column].apply(str))
     return _append_postfix_to_symbol(symbol_list, region)
 
@@ -110,6 +76,7 @@ def get_symbol_list_from_raw_html(info):
     """
 
     region, url, indices = itemgetter('region', 'url', 'indices')(info)
+
     r = requests.get(url)
     if r.status_code != 200:
         print("Error fetching page")
@@ -117,7 +84,7 @@ def get_symbol_list_from_raw_html(info):
     soup = BeautifulSoup(r.content, "html.parser")
     links = soup.find_all('a')
 
-    symbol_list = [l.string.split(' ')[0] for l in links[indices]]
+    symbol_list = [l.string.split(' ')[0] for l in links[slice(*indices)]]
     return _append_postfix_to_symbol(symbol_list, region)
 
 
@@ -136,10 +103,12 @@ def write_to_file(output_dir, market_name, symbol_list, seperator='\n'):
     fname = os.path.join(os.path.join(output_dir, "%s.txt" % market_name))
     open(fname, 'w').write(seperator.join(symbol_list))
 
+
 def _append_postfix_to_symbol(symbol_list, region, postfix = '.T'):
     if region == 'jp':
         symbol_list = [ i + postfix for i in symbol_list]
     return symbol_list
+
 
 if __name__ == '__main__':
     main()
